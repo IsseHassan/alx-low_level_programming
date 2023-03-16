6380e52feefa3bea05a7f609a7256286b86195d8
IsseHassan <cadeysey14@gmail.com>
2023-03-16 15:33:44 -0700

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#!/bin/bash
+#include <stdio.h>
+
+int main(void)
+{
+    // Use the function puts to print the string
+    puts("Programming is like building a multilingual puzzle");
+    // Return 0 to indicate success
+    return (0);
+}
