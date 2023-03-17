6c39217e286e55bb7a67bb2b723e7b9725acf486
IsseHassan <cadeysey14@gmail.com>
2023-03-17 14:59:01 -0700

    quoto.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,6 +4,7 @@
  * Return: 1 (Success)
  */
 int main(void)
-{ write(stderr, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+{
+       	write(stderr, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
        	return (1);
 }
