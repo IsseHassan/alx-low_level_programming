90324bd10cc79de0c1e94164135a3aa9fa7f2044
IsseHassan <cadeysey114@gmail.com>
2023-03-17 15:19:07 -0700

    quot.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
-
+/**
+ * main - A program that prints a line to the standrad error
+ * Return: 1 (Success)
+ */
 int main(void)
 {
     char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
