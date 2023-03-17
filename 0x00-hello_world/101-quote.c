d549e912de90c805197e9438ab92e27354ede713
Isse Hassan Mohamed <121172027+IsseHassan@users.noreply.github.com>
2023-03-18 01:28:33 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A program that prints a line to the standrad error
- * Return: 1 (Success)
- */
-int main(void)
-{
-    char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+*main - A program that prints a line to the standrad error
+*Return: 1 (Success) 
+*/ 
+int main(void) 
+{ 
+    char str[] = “and that piece of art is useful" - Dora Korpar, 2015-10-19\n”;
 
     write(2, str, sizeof(str) - 1);
 
-    return (1);
+    return (1); 
 }
