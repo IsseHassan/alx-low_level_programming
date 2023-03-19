621ee12532c31ff875310faf2685f15d928aeed7
IsseHassan <cadeysey114@gmail.com>
2023-03-19 11:35:56 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -4,6 +4,7 @@
 /**
  * main - prints all possible different combinations of three digits.
  * Numbers must be separated by ,
+ * Followed by new line
  * Return: Always 0 (success)
  */
 int main(void)
@@ -11,24 +12,24 @@ int main(void)
 	int d, p, l;
 
 	for (d = '0'; d < '9'; d++)
+	{
+		for (p = d + 1; p <= '9'; p++)
 		{
-			for (p = d + 1; p <= '9'; p++)
+			for (l = p + 1; l <= '9'; l++)
 			{
-				for (l = p + 1; l <= '9'; l++)
+				if (p != d && l != p)
 				{
-					if (p != d && l != p)
-						{
-							putchar(d);
-							putchar(p);
-							putchar(l);
-							if (d == '8' && p == '9')
-								continue;
-							putchar(',');
-							putchar(' ');
-						}
-					}
+					putchar(d);
+					putchar(p);
+					putchar(l);
+					if (d == '8' && p == '9')
+						continue;
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
+	}
 	putchar('\n');
 	return (0);
 }
