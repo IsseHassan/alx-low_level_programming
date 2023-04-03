faac76f816dbc7473a927fb461b1b1ccd10cd925
IsseHassan <cadeysey114@gmail.com>
2023-04-03 11:54:03 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,12 +8,15 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0' && *s != c)
+
+	while (*s != '\0')
 	{
+		if (*s == c)
+			return (s);
 		s++;
 	}
-	if (*s == c)
-	{
+	if (c != '\0')
 		return (s);
-	}
+	return (0);
+
 }
