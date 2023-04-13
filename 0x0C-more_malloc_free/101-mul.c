6db4e892654a416da415faa7cb9be97c7a4176a5
IsseHassan <cadeysey114@gmail.com>
2023-04-13 07:38:03 -0700

    101-mul.c

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int num1, num2;
+
+    if (argc != 3) {
+        printf("Error\n");
+        return (98);
+    }
+
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[2]);
+
+    if (num1 == 0 || num2 == 0) {
+        printf("Error\n");
+        return (98);
+    }
+
+    printf("%d\n", num1 * num2);
+
+    return (0);
+}
